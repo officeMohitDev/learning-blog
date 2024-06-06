d1a5f8d3c3eab6a4b95c841bd13879518c41b8d4
developerdesigner18 <developer.designer18@gmail.com>
2024-06-06 09:57:26 +0530

    add navbar

--- frotend/src/app/page.tsx
+++ frotend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+<div className="w-screen h-screen flex justify-center items-center">
+    <Button>Button</Button>
+</div>
   );
 }
