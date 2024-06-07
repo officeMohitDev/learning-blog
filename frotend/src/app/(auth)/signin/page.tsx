904c0fd2fec1581869ff85c9c84d2186e8b7690c
developerdesigner18 <developer.designer18@gmail.com>
2024-06-07 13:01:07 +0530

    added input shadcn

--- frotend/src/app/(auth)/signin/page.tsx
+++ frotend/src/app/(auth)/signin/page.tsx
@@ -29,70 +29,42 @@ const SignInPage = () => {
 
   return (
     <motion.div
-      className="flex flex-col items-center justify-center min-h-screen w-full p-4"
       initial="initial"
       animate={isMounted ? "animate" : "initial"}
       variants={pageVariants}
     >
-      <div className="flex items-center justify-between w-full mb-6">
+      <div className="flex items-center justify-between w-full mb-6 px-12 mt-6">
         <Link href={"/"} className="text-gray-500">&larr; Back</Link>
         <button className="text-gray-500">&times;</button>
       </div>
-      <div className="w-full max-w-3xl bg-white rounded-lg p-8">
-        <h1 className="text-4xl font-bold text-center mb-8">Kahi Suni 💮</h1>
-        <form className="space-y-3">
-          <div className="mb-1 text-left">
-            <label htmlFor="email" className="block mb-2 font-bold">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="mb-1 text-left">
-            <label htmlFor="email" className="block mb-2 font-bold">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="w-full p-2 border border-gray-300 rounded"
-            />
-          </div>
-          <div className="flex items-center justify-center space-x-4 mb-6">
-            <button
-              type="button"
-              className={`px-4 py-2 rounded-full ${isMonthly ? 'bg-gray-200' : 'bg-white'}`}
-              onClick={() => setIsMonthly(true)}
-            >
-              Monthly
-            </button>
-            <button
-              type="button"
-              className={`px-4 py-2 rounded-full ${!isMonthly ? 'bg-gray-200' : 'bg-white'}`}
-              onClick={() => setIsMonthly(false)}
-            >
-              Yearly
-            </button>
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <div className="border rounded-lg p-6 text-start">
-              <h2 className="text-xl font-bold text-red-600">Free</h2>
-              <p className="text-2xl font-bold mt-2">$0</p>
-              <p className="text-gray-600 mt-2">Free preview</p>
-              <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded-md">Choose</button>
+      <div className="flex flex-col items-center justify-center min-h-screen w-full p-4">
+        <div className="w-full max-w-3xl bg-white rounded-lg p-8">
+          <h1 className="text-4xl font-bold text-center mb-8">Kahi Suni 💮 - Sign in</h1>
+          <form className="space-y-3">
+            <div className="mb-1 text-left">
+              <label htmlFor="email" className="block mb-2 font-bold">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
             </div>
-            <div className="border rounded-lg p-6 text-start">
-              <h2 className="text-xl font-bold text-red-600">Penang</h2>
-              <p className="text-2xl font-bold mt-2">$5 <span className="text-sm text-gray-600">/month</span></p>
-              <p className="text-gray-600 mt-2">Full access</p>
-              <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded-md">Continue</button>
+            <div className="mb-1 text-left">
+              <label htmlFor="email" className="block mb-2 font-bold">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
             </div>
-          </div>
-          <p className="text-center mt-6">
-            Dont have an Account? <Link href="/signup" className="text-red-600">Sign up</Link>
-          </p>
-        </form>
+
+            <p className="text-center mt-6">
+              Dont have an Account? <Link href="/signup" className="text-red-600">Sign up</Link>
+            </p>
+          </form>
+        </div>
       </div>
     </motion.div>
   )
--- frotend/src/app/(auth)/signup/page.tsx
+++ frotend/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,106 @@
-import React from 'react'
+"use client"
+import Link from 'next/link';
+import { motion } from "framer-motion"
+import React, { useEffect, useState } from 'react'
+import { Label } from '@radix-ui/react-label';
+import { Input } from '@/components/ui/input';
+import '../index.css'
 
 const SignUpPage = () => {
+  const [isMonthly, setIsMonthly] = useState(true);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  const pageVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   return (
-      <div>Hello</div>
+    <motion.div
+      className="flex flex-col items-center justify-center min-h-screen w-full p-4"
+      initial="initial"
+      animate={isMounted ? "animate" : "initial"}
+      variants={pageVariants}
+    >
+      <div className="flex items-center justify-between w-full mb-6">
+        <Link href={"/"} className="text-gray-500">&larr; Back</Link>
+        <button className="text-gray-500">&times;</button>
+      </div>
+      <div className="w-full max-w-3xl bg-white rounded-lg p-8">
+        <h1 className="text-4xl font-bold text-center mb-8">Kahi Suni 💮 - Register</h1>
+        <p className='text-2xl font-semibold text-center'></p>
+        <form className="space-y-3 w-full ">
+          <div className='flex gap-3 w-full'>
+            <div className="mb-1 text-left w-full">
+              <Label htmlFor="email">Name</Label>
+              <Input type="text" id="name" placeholder="name" className='outline-none w-full' />
+            </div>
+            <div className="mb-1 text-left w-full">
+              <Label htmlFor="email">Username</Label>
+              <Input type="text" id="username" placeholder="username" className='outline-none w-full' />
+            </div>
+          </div>
+          <div className='flex gap-3 w-full'>
+            <div className="mb-1 text-left w-full">
+              <Label htmlFor="email">Email</Label>
+              <Input type="email" id="email" placeholder="Email" className='outline-none w-full' />
+            </div>
+            <div className="mb-1 text-left w-full">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" placeholder="password" className='outline-none w-full' />
+            </div>
+          </div>
+          <div className="flex items-center justify-center space-x-4 mb-6">
+            <button
+              type="button"
+              className={`px-4 py-2 rounded-full ${isMonthly ? 'bg-gray-200' : 'bg-white'}`}
+              onClick={() => setIsMonthly(true)}
+            >
+              Monthly
+            </button>
+            <button
+              type="button"
+              className={`px-4 py-2 rounded-full ${!isMonthly ? 'bg-gray-200' : 'bg-white'}`}
+              onClick={() => setIsMonthly(false)}
+            >
+              Yearly
+            </button>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div className="border rounded-lg p-6 text-start">
+              <h2 className="text-xl font-bold text-red-600">Free</h2>
+              <p className="text-2xl font-bold mt-2">$0</p>
+              <p className="text-gray-600 mt-2">Free preview</p>
+              <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded-md">Choose</button>
+            </div>
+            <div className="border rounded-lg p-6 text-start">
+              <h2 className="text-xl font-bold text-red-600">Pro</h2>
+              <p className="text-2xl font-bold mt-2">$5 <span className="text-sm text-gray-600">/month</span></p>
+              <p className="text-gray-600 mt-2">Full access</p>
+              <button className="mt-4 bg-red-600 text-white py-2 px-4 rounded-md">Continue</button>
+            </div>
+          </div>
+          <p className="text-center mt-6">
+            Dont have an Account? <Link href="/signin" className="text-red-600">Sign In</Link>
+          </p>
+        </form>
+      </div>
+    </motion.div>
   )
 }
 
