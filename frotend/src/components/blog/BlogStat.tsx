02f9f0aa2bde75e2f3c8b48df881638e87926e81
developerdesigner18 <developer.designer18@gmail.com>
2024-06-15 12:50:44 +0530

    add reply functionality

--- frotend/src/components/blog/BlogStat.tsx
+++ frotend/src/components/blog/BlogStat.tsx
@@ -6,12 +6,16 @@ import { baseURL } from '@/constants';
 import { toast } from 'sonner';
 import { formateBlogDate } from '@/utils/datefun';
 import { formatComments } from '@/lib/utils';
+import { Button } from '../ui/button';
 
 const BlogStat = ({ blog: blogData, user }: { blog: any, user: any }) => {
     const [isLiked, setIsLiked] = useState(blogData.likes.find((like: any) => like._id === user._id));
     const [blog, setBlog] = useState(blogData);
     const [formattedComments, setFormattedComments] = useState<any[]>([]);
-    const [commentMsg, setCommentMsg] = useState("")
+    const [replyBox, setReplyBox] = useState(false);
+    const [topCommentId, setTopCommentId] = useState("")
+    const [commentMsg, setCommentMsg] = useState("");
+    const [replyMsg, setReplyMsg] = useState("")
 
     useEffect(() => {
         if (blog.comments) {
@@ -71,6 +75,37 @@ const BlogStat = ({ blog: blogData, user }: { blog: any, user: any }) => {
             console.log(error)
         }
     }
+    const sendReply = async (e: FormEvent) => {
+        e.preventDefault();
+        if (replyMsg === "") return
+        try {
+            const res = await fetch(`${baseURL}/comment/create/${blog._id}`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${user?._id}`,
+                },
+                body: JSON.stringify({
+                    topCommentId,
+                    isNested: true,
+                    commentMsg: replyMsg,
+                    pinned: false
+                })
+            })
+            if (res.ok) {
+                toast.success("reply added");
+                const data = await res.json();
+                console.log("commented blog", data)
+                setBlog(data.blog);
+                setReplyMsg("");
+                setTopCommentId("");
+                setReplyBox(false);
+            }
+            return
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div className="max-w-3xl mx-auto p-4">
@@ -95,13 +130,14 @@ const BlogStat = ({ blog: blogData, user }: { blog: any, user: any }) => {
                 <h2 className="text-xl font-bold">{blog.comments.length} Comment</h2>
                 <div className="mt-2 flex items-start space-x-2">
                     <img src={user.image} alt="User avatar" className="w-12 h-12 rounded-full" />
-                    <form className='w-full' onSubmit={sendComment}>
-                        <input
-                            className="flex-1 w-full border rounded p-2"
+                    <form className='w-full flex flex-col gap-3 items-end ml-4' onSubmit={sendComment}>
+                        <textarea
+                            className="flex-1 border w-full rounded p-2"
                             placeholder="Write a comment..."
                             value={commentMsg}
                             onChange={e => setCommentMsg(e.target.value)}
                         />
+                        <Button type='submit' className=''>Comment</Button>
                     </form>
 
                 </div>
@@ -119,11 +155,27 @@ const BlogStat = ({ blog: blogData, user }: { blog: any, user: any }) => {
                                 </div>
                                 <div className="flex space-x-2 text-gray-500">
                                     <button>Like</button>
-                                    <button>Reply</button>
+                                    <button onClick={() => {
+                                        setReplyBox(!replyBox);
+                                        setTopCommentId(comment._id)
+                                    }}>Reply</button>
                                     <button>Share</button>
                                 </div>
                             </div>
                             <p className="mt-2">{comment?.comment}</p>
+                            {
+                                replyBox && topCommentId === comment._id && (
+                                    <form className='w-full flex flex-col gap-3 items-end ml-4' onSubmit={sendReply}>
+                                        <textarea
+                                            className="flex-1 border w-full rounded p-2"
+                                            placeholder="Write a Reply..."
+                                            value={replyMsg}
+                                            onChange={e => setReplyMsg(e.target.value)}
+                                        />
+                                        <Button type="submit">Reply</Button>
+                                    </form>
+                                )
+                            }
                             {comment?.replies?.length > 0 && (
                                 <div className="ml-1 mt-2 space-y-2">
                                     {comment?.replies?.map((reply: any) => (
